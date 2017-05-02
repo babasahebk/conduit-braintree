20223b7c00dcb373a82c2cb45fe94e557676b445
beardo <jason.moore@hellolabs.com>
2017-05-02 08:11:14 -0600

    Client token options (#40)

--- lib/conduit/braintree/actions/client_token.rb
+++ lib/conduit/braintree/actions/client_token.rb
@@ -1,9 +1,11 @@
 module Conduit::Driver::Braintree
   class ClientToken < Base
+    optional_attributes :customer_id, :merchant_account_id
+
     private
 
     def perform_request
-      response = Braintree::ClientToken.generate
+      response = Braintree::ClientToken.generate(whitelist_options)
       body = { client_token: response, successful: true }.to_json
       parser = parser_class.new(body)
       Conduit::ApiResponse.new(raw_response: response, body: body, parser: parser)
