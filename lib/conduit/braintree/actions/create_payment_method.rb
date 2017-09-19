7cb37b99b28010c1e0658f40c32791dde8ced10c
babasahebk <baba.kardel@hellolabs.com>
2017-09-19 10:29:48 -0400

    Fix braintree error handling (#43)
    * Fix error response message, Response will have errors only if there is validation failure, the message contains detailed human readable error description.

--- lib/conduit/braintree/actions/create_payment_method.rb
+++ lib/conduit/braintree/actions/create_payment_method.rb
@@ -5,22 +5,47 @@ module Conduit::Driver::Braintree
     # To create a new payment method for an existing customer,
     # the only required attributes are the customer ID and payment method nonce.
     required_attributes :payment_method_nonce, :customer_id
+    attr_accessor :raw_response
 
     private
 
     def perform_request
-      response = Braintree::PaymentMethod.create(whitelist_options)
-      if response.success?
-        credit_card_response = OpenStruct.new(credit_card: response.payment_method)
-        body = Conduit::Driver::Braintree::Json::CreditCard.new(credit_card_response).to_json
-
-        parser = parser_class.new(body)
-        Conduit::ApiResponse.new(raw_response: response, body: body, parser: parser)
+      @raw_response = Braintree::PaymentMethod.create(whitelist_options)
+      if success?
+        generate_response(credit_card_response)
       else
-        respond_with_error(response.errors.map(&:message).join("."))
+        generate_response(error_response)
       end
     rescue Braintree::BraintreeError => error
       report_braintree_exceptions(error)
+    end
+
+    def success?
+      raw_response.success?
+    end
+
+    def credit_card_response
+      OpenStruct.new(credit_card: raw_response.payment_method)
+    end
+
+    def error_response
+      OpenStruct.new(success?: false, errors: [OpenStruct.new(attribute: :base, code: "error", 
+        message: error_message)])
+    end
+
+    def error_message
+      message = raw_response.errors.map { |error| "#{error.code}: #{error.message}" }.join(".")
+      if raw_response.message.present?
+        message.concat(". ") if message.present?
+        message.concat(raw_response.message)
+      end
+      message
+    end
+
+    def generate_response(response_body)
+      body = Conduit::Driver::Braintree::Json::CreditCard.new(response_body).to_json
+      parser = parser_class.new(body)
+      Conduit::ApiResponse.new(raw_response: raw_response, body: body, parser: parser)
     end
   end
 end
