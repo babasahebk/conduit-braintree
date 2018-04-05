b89e562cf895b7a733364b5b2d16647f66d791c5
Doug <doug.perez@hellolabs.com>
2018-04-05 17:12:32 -0400

    add hard error codes

--- lib/conduit/braintree/parsers/base.rb
+++ lib/conduit/braintree/parsers/base.rb
@@ -3,23 +3,6 @@ require "multi_json"
 module Conduit::Driver::Braintree
   module Parser
     class Base < Conduit::Core::Parser
-      HARD_ERROR_CODES = [
-       "2004", "2005", "2006", "2007", "2008",
-       "2010", "2011", "2012", "2013", "2014",
-       "2015", "2017", "2018", "2019", "2020",
-       "2022", "2023", "2024", "2027", "2028",
-       "2029", "2030", "2031", "2032", "2036",
-       "2037", "2039", "2041", "2043", "2044",
-       "2045", "2047", "2049", "2051", "2053",
-       "2055", "2056", "2058", "2059", "2060",
-       "2061", "2063", "2064", "2065", "2066",
-       "2067", "2068", "2069", "2070", "2071",
-       "2072", "2073", "2074", "2075", "2076",
-       "2077", "2079", "2081", "2082", "2083",
-       "2084", "2085", "2086", "2087", "2088",
-       "2089", "2090", "2091"
-      ].freeze
-
       attr_reader :json, :http_status
 
       def initialize(response_body, http_status = nil)
@@ -42,7 +25,7 @@ module Conduit::Driver::Braintree
 
       def hard_error_message?
         msg = errors.map(&:message).join(",")
-        HARD_ERROR_CODES.any? { |code| msg.match?(code) }
+        Conduit::Braintree.hard_error_codes.any? { |code| msg.match?(code) }
       end
 
       # Returns a hash of errors reported by Braintree
