c0d7183f06fc9c3101b6f15fa7973f6b05cac492
Pedro Martinez <pedro.martinez@hellolabs.com>
2019-05-09 16:55:53 -0400

    Passing along transaction source

--- lib/conduit/braintree/actions/authorize_transaction.rb
+++ lib/conduit/braintree/actions/authorize_transaction.rb
@@ -8,10 +8,6 @@ module Conduit::Driver::Braintree
 
     private
 
-    # Required entry method, the main driver
-    # class will use this to trigger the
-    # request.
-    #
     def perform_request
       parameters = {
         amount: @options[:amount],
@@ -20,9 +16,9 @@ module Conduit::Driver::Braintree
           skip_advanced_fraud_checking: !@options[:enable_fraud_checking]
         }
       }
+      parameters[:transaction_source] = @options[:transaction_source] if @options[:transaction_source].present?
       parameters[:merchant_account_id] = @options[:merchant_account_id] if @options[:merchant_account_id].present?
       parameters[:device_data] = @options[:device_data] if @options[:device_data].present?
-
       @raw_response = Braintree::Transaction.sale(parameters)
       Conduit::Driver::Braintree::Json::Transaction.new(@raw_response).to_json
     end
